75a87070e26abb26ee13761d9004399b8d92a031
sebastian Montes <u201712366@upc.edu.pe>
2023-09-26 10:30:02 -0500

    feat: changes json server

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Ruta al archivo JSON de datos
-
+const jsonServer= require("json-server");
+const server= jsonServer.create();
+const router= jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001
 
-// Verificar si estamos en el entorno de desarrollo local
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 4000; // Usar un puerto diferente en desarrollo
-  server.use(middlewares);
-  server.use(router);
+server.use(middlewares);
+server.use(router);
 
-  server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${PORT}`);
-  });
-}
+server.listen(port);
